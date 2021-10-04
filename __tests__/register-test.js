235e25f500b706b3ff6b85040e28f573d540e7bb
Abdelrahman84 <abdelrahman.farag84@gmail.com>
2021-10-04 19:14:29 +0200

    adding unit test for register compnent

--- __tests__/register-test.js
+++ __tests__/register-test.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import {render} from '@testing-library/react-native'
+import { render, fireEvent, waitFor } from '@testing-library/react-native'
 
 import Register from '../components/register';
 
-test('Register page should render', () => {
-    const {getByText} = render(<Register />)
+describe('Register page', () => {
 
-    const registerBtn = getByText('Register');
-    expect(registerBtn).not.toBeNull();
+
+    test('Register page should render', async () => {
+
+        const { getByTestId, getByPlaceholderText } = render(<Register />)
+
+        const registerBtn = getByTestId('register-btn');
+        await waitFor(() => {
+
+            expect(registerBtn).not.toBeNull();
+            expect(registerBtn).toBeDisabled();
+
+            const firstNameInput = getByPlaceholderText('fist name');
+            const lastNameInput = getByPlaceholderText('last name');
+            const usernameInput = getByPlaceholderText('username');
+            const emailInput = getByPlaceholderText('email');
+            const phoneNumberInput = getByPlaceholderText('phone number');
+
+            fireEvent.changeText(firstNameInput, 'Ahmed');
+            fireEvent.changeText(lastNameInput, 'Mohamed');
+            fireEvent.changeText(usernameInput, 'abdu');
+            fireEvent.changeText(emailInput, 'test@test.com');
+            fireEvent.changeText(phoneNumberInput, '0123456789');
+
+            expect(registerBtn).not.toBeDisabled();
+        })
+    })
 })
