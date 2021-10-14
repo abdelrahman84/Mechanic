9252dd153bcf8f9873aa8d262256c75b36703ba7
Abdelrahman84 <abdelrahman.farag84@gmail.com>
2021-10-14 20:42:03 +0200

    refacor register component, and adding unit test  for register component

--- __tests__/register-test.js
+++ __tests__/register-test.js
@@ -1,34 +1,50 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react-native'
+import { render, fireEvent, waitFor, act } from '@testing-library/react-native'
 
-import Register from '../components/register';
+import RegisterForm from '../UI_Components/registerForm';
 
-describe('Register page', () => {
+describe('Register form', () => {
 
+    test('Register form should render correctly', async () => {
 
-    test('Register page should render', async () => {
+        navigateToLogin = jest.fn();
+        formSumbit = jest.fn();
+        navigateToLogin = jest.fn();
 
-        const { getByTestId, getByPlaceholderText } = render(<Register />)
+        const { queryByTestId, getByPlaceholderText, queryByText } = render(<RegisterForm onNavigateToLogin={navigateToLogin} onFormSumbit={formSumbit}
+            isLoading={false} />)
 
-        const registerBtn = getByTestId('register-btn');
+        const registerBtn = queryByTestId('register-btn');
+        const loginLink = queryByText('Login');
+
         await waitFor(() => {
-
             expect(registerBtn).not.toBeNull();
+            expect(loginLink).not.toBeNull();
             expect(registerBtn).toBeDisabled();
-
-            const firstNameInput = getByPlaceholderText('fist name');
-            const lastNameInput = getByPlaceholderText('last name');
-            const usernameInput = getByPlaceholderText('username');
-            const emailInput = getByPlaceholderText('email');
-            const phoneNumberInput = getByPlaceholderText('phone number');
-
-            fireEvent.changeText(firstNameInput, 'Ahmed');
-            fireEvent.changeText(lastNameInput, 'Mohamed');
-            fireEvent.changeText(usernameInput, 'abdu');
-            fireEvent.changeText(emailInput, 'test@test.com');
-            fireEvent.changeText(phoneNumberInput, '0123456789');
-
-            expect(registerBtn).not.toBeDisabled();
         })
+
+        const firstNameInput = getByPlaceholderText('first name');
+        const lastNameInput = getByPlaceholderText('last name');
+        const usernameInput = getByPlaceholderText('username');
+        const emailInput = getByPlaceholderText('email');
+        const phoneNumberInput = getByPlaceholderText('phone number');
+
+        await act(async () =>
+            fireEvent.changeText(firstNameInput, 'Ahmed'),
+            fireEvent.changeText(lastNameInput, 'Mohamed'),
+            fireEvent.changeText(usernameInput, 'abdu'),
+            fireEvent.changeText(emailInput, 'test@test.com'),
+            fireEvent.changeText(phoneNumberInput, '01234567891'),
+        )
+        expect(registerBtn).not.toBeDisabled();
+
+        await act(async () =>
+            fireEvent.press(registerBtn));
+        expect(formSumbit).toHaveBeenCalled();
+
+        // test for login link
+        fireEvent.press(loginLink);
+        expect(navigateToLogin).toHaveBeenCalled();
+
     })
 })
--- components/register.js
+++ components/register.js
@@ -1,44 +1,14 @@
 import React, { useState } from 'react';
 import type { Node } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { Dimensions } from 'react-native';
+import { View } from 'react-native';
 
 
 // 3rd party
-import { Formik } from 'formik';
-import { Input, Button } from 'react-native-elements';
-import * as yup from 'yup';
 import axios from 'axios';
 import Toast from 'react-native-toast-message';
 
 import { API_URL } from "@env";
-
-const windowHeight = Dimensions.get('window').height;
-
-
-const PHONE_NUM_MIN_DIGITS = 11;
-
-const registerValidation = yup.object().shape({
-    first_name: yup
-        .string()
-        .required('First name is required'),
-    last_name: yup
-        .string()
-        .required('Last name is required'),
-    username: yup
-        .string()
-        .required('username is required'),
-    email: yup
-        .string()
-        .email('Please enter a valid email addresss')
-        .required('Email address is required'),
-    phone: yup
-        .string()
-        .matches(/^(?=\d{11,}$)(01)\d/, 'Please enter a valid number')
-        .min(PHONE_NUM_MIN_DIGITS, ({ min }) => `Phone number must be ${min} digits`)
-        .required('Phone number is required')
-
-})
+import RegisterForm from '../UI_Components/registerForm';
 
 const Register: () => Node = ({ navigation }) => {
 
@@ -74,123 +44,15 @@ const Register: () => Node = ({ navigation }) => {
 
     return (
 
-        <View style={styles.registerContainer}>
+        <View>
 
-            <Formik
-                validationSchema={registerValidation}
-                initialValues={{ first_name: '', last_name: '', username: '', email: '', phone: '' }}
-                onSubmit={values => handleRegister(values)}
-            >
-                {({ handleChange, handleBlur, handleSubmit, values, errors, touched, isValid, dirty }) => (
-                    <View style={styles.registerForm}>
-
-                        <Input
-                            placeholder='fist name'
-                            label='First name'
-                            onChangeText={handleChange('first_name')}
-                            onBlur={handleBlur('first_name')}
-                            value={values.first_name}
-                            style={styles.input}
-                        />
-                        {(errors.first_name && touched.first_name) &&
-                            <Text style={styles.inputError}>{errors.first_name}</Text>
-                        }
-
-                        <Input
-                            placeholder='last name'
-                            label='Last name'
-                            onChangeText={handleChange('last_name')}
-                            onBlur={handleBlur('last_name')}
-                            value={values.last_name}
-                        />
-                        {(errors.last_name && touched.last_name) &&
-                            <Text style={styles.inputError}>{errors.last_name}</Text>
-                        }
-
-                        <Input
-                            placeholder='username'
-                            label='Username'
-                            onChangeText={handleChange('username')}
-                            onBlur={handleBlur('username')}
-                            value={values.username}
-                        />
-                        {(errors.username && touched.username) &&
-                            <Text style={styles.inputError}>{errors.username}</Text>
-                        }
-
-                        <Input
-                            placeholder='email'
-                            label='Email'
-                            onChangeText={handleChange('email')}
-                            onBlur={handleBlur('email')}
-                            value={values.email}
-                            keyboardType='email-address'
-                        />
-                        {(errors.email && touched.email) &&
-                            <Text style={styles.inputError}>{errors.email}</Text>
-                        }
-
-                        <Input
-                            placeholder='phone number'
-                            label='Phone number'
-                            onChangeText={handleChange('phone')}
-                            onBlur={handleBlur('phone')}
-                            value={values.phone}
-                            keyboardType='numeric'
-                        />
-                        {(errors.phone && touched.phone) &&
-                            <Text style={styles.inputError}>{errors.phone}</Text>
-                        }
-
-                        <Button
-                            title='Register'
-                            onPress={handleSubmit}
-                            buttonStyle={styles.submitBtn}
-                            disabled={!isValid || !dirty || loading}
-                            testID="register-btn"
-                        />
-                        <View style={styles.loginLinkContainer}>
-                            <Text>Already have an account? </Text>
-                            <Text style={styles.loginLink} onPress={navigateToLogin}>Login</Text>
-                        </View>
-
-                    </View>
-
-                )}
-
-            </Formik>
+        <RegisterForm onNavigateToLogin={navigateToLogin} onFormSumbit={handleRegister}
+          isLoading={loading}/>
+            
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    registerContainer: {
-        display: 'flex',
-        padding: 10,
-        backgroundColor: 'white',
-        elevation: 10,
-        flex: 1,
-    },
-    registerForm: {
-        flex: 1,
-        height: windowHeight,
-        marginTop: 'auto',
-    },
-    inputError: {
-        color: 'red',
-    },
-    submitBtn: {
-        marginTop: '10%',
-    },
-    loginLinkContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        padding: 5,
-        marginLeft: 'auto',
-    },
-    loginLink: {
-        color: 'blue'
-    }
-})
+
 
 export default Register;
