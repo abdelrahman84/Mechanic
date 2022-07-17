2065b6c420c5f822e6dd725f4fbec4af64a9ca34
Abdelrahman84 <abdelrahman.farag84@gmail.com>
2022-07-17 21:39:24 +0200

    fix to navigation issue

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import AddMechanic from './AddMechanic';
+import AddMechanic from './AddMechanic/AddMechanic';
 import Dashboard from './Dashboard';
 
 
@@ -13,28 +12,26 @@ const Tab = createBottomTabNavigator();
 const HomeComponent: () => Node = () => {
 
     return (
-        <NavigationContainer independent>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    headerShown: false,
-                    tabBarIcon: ({ color, size }) => {
-                        let iconName;
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarIcon: ({ color, size }) => {
+                    let iconName;
 
-                        if (route.name === 'Home') {
-                            iconName = 'home'
-                        }
-
-                        if (route.name === 'Add Mechanic') {
-                            iconName = 'engineering';
-                        }
-                        return <Icon name={iconName} size={size} color={color} />
+                    if (route.name === 'Home') {
+                        iconName = 'home'
                     }
-                })}>
 
-                <Tab.Screen name="Home" component={Dashboard} />
-                <Tab.Screen name="Add Mechanic" component={AddMechanic} />
-            </Tab.Navigator>
-        </NavigationContainer>
+                    if (route.name === 'Add Mechanic') {
+                        iconName = 'engineering';
+                    }
+                    return <Icon name={iconName} size={size} color={color} />
+                }
+            })}>
+
+            <Tab.Screen name="Home" component={Dashboard} />
+            <Tab.Screen name="Add Mechanic" component={AddMechanic} />
+        </Tab.Navigator>
     )
 }
 
